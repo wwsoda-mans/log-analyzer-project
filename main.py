7c7cf2e9c31916da798c87abad646e481de085a6
Bogdat Batyrkhan <batikonti@gmail.com>
2026-05-18 16:38:36 +0500

    Update main runner

--- main.py
+++ main.py
@@ -1,6 +1,31 @@
 from log_parser import parse_logs
+from analyzer import count_by_level, count_total_logs, get_top_messages, print_level_counts, print_logs
+from filters import filter_by_level, filter_by_date, filter_by_message_word
 
-logs=parse_logs("logs.txt")
-for log in logs:
-    log.show()
-    print("-----")
+logs = parse_logs("logs.txt")
+
+print("ALL LOGS")
+print_logs(logs)
+
+print("TOTAL LOGS")
+print(count_total_logs(logs))
+
+print("COUNT BY LEVEL")
+level_counts = count_by_level(logs)
+print_level_counts(level_counts)
+
+print("ERROR LOGS")
+error_logs = filter_by_level(logs, "ERROR")
+print_logs(error_logs)
+
+print("LOGS BY DATE")
+date_logs = filter_by_date(logs, "2026-05-18")
+print_logs(date_logs)
+
+print("LOGS WITH WORD")
+word_logs = filter_by_message_word(logs, "User")
+print_logs(word_logs)
+
+print("TOP MESSAGES")
+top_messages = get_top_messages(logs, 3)
+print(top_messages)
