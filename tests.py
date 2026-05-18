4d80daae89b19465cecfbcd8700bad7a27aab3da
erasy <erasylkonakbaj85@gmail.com>
2026-05-18 18:10:08 +0600

    test.py были мельки ошибки когдя я писал в грамматике , исправленая версия

--- tests.py
+++ tests.py
@@ -1,18 +1,18 @@
-from log_parser import parse_log
-from file_handler import filter_errors
+from log_parser import parse_logs
+from filters import filter_errors
 
 from utils import print_title
 
 
 def test_parser():
-    logs = parse_log("logs.txt")
+    logs = parse_logs("logs.txt")
 
     if len(logs) > 0:
         print("parser test passed")
     else:
         print("parser test failed")
 def test_filter():
-    logs = parse_log("logs.txt")
+    logs = parse_logs("logs.txt")
     errors = filter_errors(logs)
     if isinstance(errors, list):
         print("filtering test passed")
