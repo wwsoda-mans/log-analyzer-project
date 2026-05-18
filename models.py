949619fdbaf9b6142c5a55e46b6016a0f49bedf3
plikhk <alikhanmegacoolboy@gmail.com>
2026-05-18 19:05:18 +0500

    Final working version

--- models.py
+++ models.py
@@ -1,3 +1,8 @@
+class Log:
+    def __init__(self, date, level, message):
+        self.date = date
+        self.level = level
+        self.message = message
 
     def show(self):
         print(self.date)
