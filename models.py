2200a098e9825c1df6ed19ad44c7ee4ca6420690
wwsoda-mans <k.abilmansur2802@gmail.com>
2026-05-18 15:01:13 +0500

    Added show method to Log class

--- models.py
+++ models.py
@@ -3,3 +3,14 @@ class Log:
         self.date = date
         self.level = level
         self.message = message
+
+Class Log:
+    def __init__(self, date, level, message):
+        self.date = date
+        self.level = level
+        self.message = message
+
+    def show(self):
+        print(self.date)
+        print(self.level)
+        print(self.message)
